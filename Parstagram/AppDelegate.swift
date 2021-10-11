d61939fbdc7e13b7078824c543b4beaa922ff91d
Annie He <anniezhe0@gmail.com>
2021-10-11 17:17:54 -0400

    create a new project and include cocoapods

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "lHWI0hCzlxGt4iDMEXWqsVLeVbmeq3FzhRBdxrJd" // <- UPDATE
+                    $0.clientKey = "pHctUNQZsU618RuoLozsMDBUsnelPHZhCL2HlVnT" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
